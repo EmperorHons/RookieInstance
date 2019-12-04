05b5f7611617fa6abb44b56fe35d65c4e6316a31
Allen <ysk19950809@163.com>
2019-12-04 11:39:24 +0800

    Third commit

---
+++ Rookie071.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+
+l = [76543, 12345, 6543, 123, 91837, 2134, 32, 21, 43, 11, 84, 55]
+s = len(l)
+
+for x in range(s):
+    for y in range(s - x - 1):
+        if l[y] > l[y + 1]:
+            l[y], l[y + 1] = l[y + 1], l[y]
+print(l)

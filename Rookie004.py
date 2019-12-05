59a868c89759761cd58563b40c119c04ac25b34c
Allen <ysk19950809@163.com>
2019-12-05 09:28:44 +0800

    fifth commit

---
+++ Rookie004.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+
+import cmath
+
+# 正数的平方根
+num = int(input("请输入一个数字： "))
+
+num_sqrt = num ** 0.5
+
+print("{0}的平方根为{1:0.3f}".format(num, num_sqrt))
+
+
+# 负数/复数的平方根
+num = int(input("请输入一个数字： "))
+
+num_sqrt = cmath.sqrt(num)
+
+print("{0}的平方根为{1:0.3f} + {2:0.3f}j".format(num, num_sqrt.real, num_sqrt.imag))
+

5b2e2c79beca5b5401ba35f5002f9b546f58262d
Allen <ysk19950809@163.com>
2019-12-06 17:24:50 +0800

    change

---
+++ Rookie007.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+https://www.runoob.com/python3/python3-random-number.html Python 随机数生成
+"""
+import random
+
+list = []
+for i in range(0, 9):
+    s = random.randint(0, 9)
+    list.append(s)
+print(list)

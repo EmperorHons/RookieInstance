4b88683bc6a0f26d93f2e8e2e82648a4d7b3de60
EmperorHons <ysk19950809@163.com>
2019-12-08 20:44:17 +0800

    ninth commit

---
+++ Rookie009.py
@@ -0,0 +1,20 @@
+# -*- coding:utf-8 -*-
+
+
+x = input('输入x值：')
+y = input('输入y值：')
+
+# 创建临时变量，并交换
+temp = x
+x = y
+y = temp
+
+print('交换后x的值为：{}'.format(x))
+print('交换后y的值为：{}'.format(y))
+
+
+# 不使用临时变量交换
+x, y = y, x
+
+print('交换后x的值为：{}'.format(x))
+print('交换后y的值为：{}'.format(y))

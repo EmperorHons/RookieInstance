a12102328c80c6baaec0f628a824cfb73f0ac4b9
Allen <ysk19950809@163.com>
2019-12-09 09:31:44 +0800

    tenth commit

---
+++ Rookie010.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+https://www.runoob.com/python3/python3-if-example.html Python if 语句
+"""
+
+num = float(input("输入一个数字："))
+
+if num > 0:
+    print("正数")
+elif num == 0:
+    print("零")
+else:
+    print("负数")

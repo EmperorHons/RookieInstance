92cd788cde4805b60e5f200b1cc0aea583e75641
EmperorHons <ysk19950809@163.com>
2019-12-11 19:59:46 +0800

    seventh commit 随机数生成

--- Rookie007.py
+++ Rookie007.py
@@ -3,9 +3,14 @@
 https://www.runoob.com/python3/python3-random-number.html Python 随机数生成
 """
 import random
+import pysnooper
 
-list = []
-for i in range(0, 9):
-    s = random.randint(0, 9)
-    list.append(s)
-print(list)
+
+@pysnooper.snoop()
+def Random():
+    list = []
+    for i in range(0, 9):
+        s = random.randint(0, 9)
+        list.append(s)
+    print(list)
+    return list

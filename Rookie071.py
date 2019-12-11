adca6da0958c39120a397bb7e285a6e660e2e358
EmperorHons <ysk19950809@163.com>
2019-12-11 20:07:36 +0800

    71th commit 冒泡排序

--- Rookie071.py
+++ Rookie071.py
@@ -2,12 +2,17 @@
 """
 https://www.runoob.com/python3/python-bubble-sort.html  Python 冒泡排序
 """
+import pysnooper
 
-l = [76543, 12345, 6543, 123, 91837, 2134, 32, 21, 43, 11, 84, 55]
-s = len(l)
 
-for x in range(s):
-    for y in range(s - x - 1):
-        if l[y] > l[y + 1]:
-            l[y], l[y + 1] = l[y + 1], l[y]
-print(l)
+@pysnooper.snoop()
+def Sort():
+    l = [76543, 12345, 6543, 123, 91837, 2134, 32, 21, 43, 11, 84, 55]
+    s = len(l)
+
+    for x in range(s):
+        for y in range(s - x - 1):
+            if l[y] > l[y + 1]:
+                l[y], l[y + 1] = l[y + 1], l[y]
+    print(l)
+    return l

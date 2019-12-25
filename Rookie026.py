a4482e4fc7c7e84762f401d7972c9579033640ab
Allen <ysk19950809@163.com>
2019-12-25 17:38:47 +0800

    twenty-sixth commit 简单计算器实现

---
+++ Rookie026.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+https://www.runoob.com/python3/python3-calendar.html    Python 生成日历
+"""
+
+import calendar
+
+
+yy = int(input("输入年份: "))
+mm = int(input("输入月份: "))
+
+# 显示日历
+print(calendar.month(yy, mm))

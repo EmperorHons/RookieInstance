64d034ea4245fac930b97a17db1194c306c9e3c7
EmperorHons <2875514671@qq.com>
2020-06-18 22:49:51 +0800

    eighteen  commit 九九乘法表

---
+++ Rookie018.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+"""
+https://www.runoob.com/python3/python3-99-table.html  Python 九九乘法表
+"""
+
+import pysnooper
+
+十八
+@pysnooper.snoop()
+def multiplication():
+    for i in range(1, 10):
+        for j in range(1, i+1):
+            print('{}X{}={}\t'.format(j, i, i*j), end='')
+        print()
+
+
+if __name__ == '__main__':
+    multiplication()

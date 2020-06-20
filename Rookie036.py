6c1763f7b4df65908fb5519bf73f74e9b3073674
EmperorHons <2875514671@qq.com>
2020-06-20 15:01:30 +0800

    thirty-sixth commit 实现秒表功能

--- Rookie036.py
+++ Rookie036.py
@@ -12,14 +12,14 @@ print('按下回车开始计时，按下Ctrl + c 停止计时')
 while True:
 
     input("")
-    starttime = time.time()
-    print('开始', starttime)
+    start_time = time.time()
+    print('开始', start_time)
     try:
         while True:
-            print('计时: ', round(time.time() - starttime, 0), '秒', end="\r")
+            print('计时: ', round(time.time() - start_time, 0), '秒', end="\r")
             time.sleep(1)
     except KeyboardInterrupt:
         print('结束')
-        endtime = time.time()
-        print('总共的时间为:', round(endtime - starttime, 2), 'secs')
+        end_time = time.time()
+        print('总共的时间为:', round(end_time - start_time, 2), 'secs')
         break

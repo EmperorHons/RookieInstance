82ccc5bade8be607b4a3812cb29f6170fd221c24
EmperorHons <2875514671@qq.com>
2020-06-20 14:52:40 +0800

    eighteen  commit 九九乘法表

--- Rookie018.py
+++ Rookie018.py
@@ -5,8 +5,8 @@ https://www.runoob.com/python3/python3-99-table.html  Python 九九乘法表
 import pysnooper
 
 
-# @pysnooper.snoop()
-def ChengFa():
+@pysnooper.snoop()
+def multiplication():
     for i in range(1, 10):
         for j in range(1, i + 1):
             print('{}x{}={}\t'.format(j, i, i*j), end=' ')
@@ -14,4 +14,4 @@ def ChengFa():
 
 
 if __name__ == '__main__':
-    ChengFa()
+    multiplication()

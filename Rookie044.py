fe789a9baac65e14a1cd522d1a9f67b81b9408c3
EmperorHons <2875514671@qq.com>
2020-06-29 21:42:05 +0800

    Forty-Four commit 清空列表

---
+++ Rookie044.py
@@ -0,0 +1,13 @@
+# -*- coding = utf-8 -*-
+# @Time : 2020/6/29 21:36
+# @Author : EmperorHons
+# @File : Rookie044.py
+# @Software : PyCharm
+"""
+https://www.runoob.com/python3/python-clear-list.html
+Python 清空列表
+clear() 方法
+"""
+
+L = [6, 0, 4, 1]
+print(L.clear())

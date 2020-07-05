7b5700e6f16adce85a3db5850f58ecfcb843c1db
EmperorHons <2875514671@qq.com>
2020-07-05 19:54:35 +0800

    fifty commit 查找列表中最大元素

---
+++ Rookie050.py
@@ -0,0 +1,23 @@
+# -*- coding = utf-8 -*-
+# @Time : 2020/7/5 19:30
+# @Author : EmperorHons
+# @File : Rookie050.py
+# @Software : PyCharm
+"""
+https://www.runoob.com/python3/python-max-list-element.html
+Python 查找列表中最大元素
+输入 : list1 = [10, 20, 4]
+输出 : 20
+"""
+
+import pysnooper
+
+
+@pysnooper.snoop()
+def max_Number(List):
+    return max(List)
+五十
+
+if __name__ == '__main__':
+    List = [1, 5, 2, 44, 13, 26, 57, 82]
+    print(max_Number(List))

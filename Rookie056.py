76a6bb65d1a08992c34a45aa288bd68b11cc0b56
EmperorHons <2875514671@qq.com>
2020-07-13 22:46:47 +0800

    Fifty-Five commit 字符串翻转

---
+++ Rookie056.py
@@ -0,0 +1,11 @@
+# -*- coding = utf-8 -*-
+# @Time : 2020/7/13 21:08
+# @Author : EmperorHons
+# @File : Rookie056.py
+# @Software : PyCharm
+
+str = "baidu"
+print(str[::-1])
+
+str = "google"
+print(''.join(reversed(str)))

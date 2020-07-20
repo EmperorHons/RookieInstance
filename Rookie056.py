6758254f484344239c18e62d7bfa7fbf9be3c69e
EmperorHons <2875514671@qq.com>
2020-07-20 22:54:44 +0800

    Fifty-Six commit 对字典进行排序

--- Rookie056.py
+++ Rookie056.py
@@ -3,6 +3,11 @@
 # @Author : EmperorHons
 # @File : Rookie056.py
 # @Software : PyCharm
+"""
+https://www.runoob.com/python3/python-string-reverse.html
+Python 字符串翻转
+给定一个字符串，然后将其翻转，逆序输出。
+"""
 
 str = "baidu"
 print(str[::-1])

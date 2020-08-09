1278becc426c3edf93c27a3bf0397d1aca207bf2
EmperorHons <2875514671@qq.com>
2020-08-09 22:16:57 +0800

    Sixty-Six commit 线性查找

--- Rookie066.py
+++ Rookie066.py
@@ -1,35 +1,37 @@
 # -*- coding = utf-8 -*-
-# @Time : 2020/8/5 22:38
+# @Time : 2020/7/30 21:44
 # @Author : EmperorHons
-# @File : Rookie066.py
+# @File : Rookie.py
 # @Software : PyCharm
 """
-https://www.runoob.com/python3/python-insertion-sort.html
-Python 插入排序
-插入排序（英语：Insertion Sort）是一种简单直观的排序算法。
-它的工作原理是通过构建有序序列，对于未排序数据，
-在已排序序列中从后向前扫描，找到相应位置并插入。
+https://www.runoob.com/python3/python-linear-search.html
+Python 线性查找
+线性查找指按一定的顺序检查数组中每一个元素，直到找到所要寻找的特定值为止。
 """
 
 import pysnooper
 
 
 @pysnooper.snoop()
-def insertionSort(arr):
-    for i in range(1, len(arr)):
-        key = arr[i]
+def search(arr, n, x):
 
-        j = i - 1
-        while j >= 0 and key < arr[j]:
-            print("比较前的key:", key)
-            arr[j + 1] = arr[j]
-            j -= 1
-        arr[j + 1] = key
-        print("比较后的key:", key)
+    for i in range(0, n):
+        if arr[i] == x:
+            return i
+    return -1;
 
 
-arr = [12, 11, 27, 41, 22, 94]
-insertionSort(arr)
-print("排序后的数组：")
-for i in range(len(arr)):
-    print("%d" % arr[i])
+def main():
+    # 在数组 arr 中查找数字10
+    arr = [1, 14, 21, 32, 81, 10, 12]
+    x = 10
+    n = len(arr)
+    result = search(arr, n, x)
+    if result == -1:
+        print("元素不在数组中")
+    else:
+        print("元素在数组中的索引为：", result)
+
+
+if __name__ == '__main__':
+    main()

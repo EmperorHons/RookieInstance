4954311fc1fee95a0cd630b10e28e90d4a52f822
EmperorHons <2875514671@qq.com>
2020-08-11 22:54:58 +0800

    Revert "Sixty-Eight commit 快速排序"
    This reverts commit d2f6b3b2

--- Rookie068.py
+++ Rookie068.py
@@ -1,61 +1,38 @@
 # -*- coding = utf-8 -*-
-# @Time : 2020/8/11 21:50
+# @Time : 2020/8/10 21:49
 # @Author : EmperorHons
 # @File : Rookie068.py
 # @Software : PyCharm
 """
-https://www.runoob.com/python3/python-quicksort.html
-Python 快速排序
-快速排序使用分治法（Divide and conquer）策略来把一个序列（list）分为
-较小和较大的2个子序列，然后递归地排序两个子序列。
-步骤为：
-挑选基准值：从数列中挑出一个元素，称为"基准"（pivot）;
-分割：重新排序数列，所有比基准值小的元素摆放在基准前面，
-所有比基准值大的元素摆在基准后面（与基准值相等的数可以到任何一边）。
-在这个分割结束之后，对基准值的排序就已经完成;
-递归排序子序列：递归地将小于基准值元素的子序列和大于基准值元素的子序列排序。
-递归到最底部的判断条件是数列的大小是零或一，此时该数列显然已经有序。
-选取基准值有数种具体方法，此选取方法对排序的时间性能有决定性影响。
+https://www.runoob.com/python3/python-selection-sort.html
+Python 选择排序
+选择排序（Selection sort）是一种简单直观的排序算法。它的工作原理如下。
+首先在未排序序列中找到最小（大）元素，存放到排序序列的起始位置，
+然后，再从剩余未排序元素中继续寻找最小（大）元素，然后放到已排序序列的末尾。
+以此类推，直到所有元素均排序完毕。
 """
+
 import pysnooper
 
 
 @pysnooper.snoop()
-def partition(arr, low, high):
-    i = (low - 1)  # 最小元素索引
-    pivot = arr[high]
+def Select_sort(List):
+    for i in range(len(List)):
+        min_dix = i
+        # print("i的值", i)
+        for j in range(i+1, len(List)):
+            # print("j的值", j)
+            if List[min_dix] > List[j]:
+                min_dix = j
 
-    for j in range(low, high):
-        # 当前元素小于或等于 pivot
-        if arr[j] <= pivot:
-            i = i + 1
-            arr[i], arr[j] = arr[j], arr[i]
-
-    arr[i+1], arr[high] = arr[high], arr[i+1]
-    return i+1
-
-# arr[] --> 排序数组
-# low  --> 起始索引
-# high  --> 结束索引
-
-
-# 快速排序函数
-@pysnooper.snoop()
-def quickSort(arr, low, high):
-    if low < high:
-        pi = partition(arr, low, high)
-        quickSort(arr, low, pi-1)
-        quickSort(arr, pi+1, high)
-
-
-def main():
-    arr = [10, 7, 8, 9, 1, 5]
-    n = len(arr)
-    quickSort(arr, 0, n-1)
-    print("排序后的数组:")
-    for i in range(n):
-        print("%d" % arr[i])
+        List[i], List[min_dix] = List[min_dix], List[i]
+    print("排序后的数组：")
+    for i in range(len(List)):
+        print("%d" % List[i]),
+    return List[i]
 
 
 if __name__ == '__main__':
-    main()
+    List = [64, 25, 12, 22, 11]
+    Select_sort(List)
+
